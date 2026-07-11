465c3659c70d137c9ae205f81a3e2625ea677c41
jaime <jaimitopele@gmail.com>
2026-07-11 11:05:18 +0200

    Added the folder day 12 Scope and number Guessing game

---
+++ day 12 Scope and number guessing game/scope.py
@@ -0,0 +1,39 @@
+apples=1
+def increaseApples():
+    apples=2
+    print(f"Apples inside the function: {apples}")
+
+
+increaseApples()
+
+print(f"Apples outside the function: {apples}")
+
+#Local Scope
+# Variaveis declaradas dentro de funcoes tem escopo local. Elas sao vistas apenas por outro codigo que esteja dentro do escopo da funcao
+
+def drinkPotion():
+    potionStrenght=2
+    print(potionStrenght)
+
+drinkPotion()
+#print(posionStrenght) This will cause a NameError because the postionStrenght variable is not declared as a global scope.
+
+
+#Global scope
+
+pontos = 0
+print("Variaveis globais")
+def ganhar_ponto():
+    #pontos = pontos + 1   # ❌ UnboundLocalError, a variavel ainda nao foi definida localmente
+    print(pontos)
+
+ganhar_ponto()
+
+#Para modificar uma variavel global dentro de uma funcao usa-se a palavra chave "global" antes do nome da variavel
+def ganhar_ponto2():
+    global pontos 
+    pontos = pontos + 1   # ❌ UnboundLocalError, a variavel ainda nao foi definida localmente
+    print(pontos)
+
+ganhar_ponto2()
+ganhar_ponto2()
